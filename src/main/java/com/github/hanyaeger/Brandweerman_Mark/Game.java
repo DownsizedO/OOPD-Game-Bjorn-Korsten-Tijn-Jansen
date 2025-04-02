100f647f1ca4b89d1ae5ad56cc64bba0b9d3069b
BjornKorsten <bma.korsten@student.han.nl>
2025-04-02 14:04:31 +0200

    Bugfixed

--- src/main/java/com/github/hanyaeger/Brandweerman_Mark/Game.java
+++ src/main/java/com/github/hanyaeger/Brandweerman_Mark/Game.java
@@ -1,14 +1,59 @@
 package com.github.hanyaeger.Brandweerman_Mark;
 
+import com.github.hanyaeger.api.YaegerGame;
+import com.github.hanyaeger.api.Size;
+
+import com.github.hanyaeger.Brandweerman_Mark.scenes.menus.Startscherm;
+import com.github.hanyaeger.Brandweerman_Mark.scenes.menus.Eindscherm;
+import com.github.hanyaeger.Brandweerman_Mark.scenes.rooms.Normal_Room;
+import com.github.hanyaeger.Brandweerman_Mark.scenes.rooms.Rooms;
+import com.github.hanyaeger.Brandweerman_Mark.entities.player.Player;
+
 import java.util.List;
 
-public interface Game {
+public class Game extends YaegerGame {
 
-    void startScreen();
-    Eindscherm endScreen();
-    List<Rooms> rooms();
-    Player player();
-    Statistics stats();
-    int ronde();
-    int winst_level();
+    private Startscherm startScreen;
+    private Eindscherm endScreen;
+    private List<Rooms> rooms;
+    private Player _player;
+    private int ronde;
+    private int winst_level;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    protected void setupGame() {
+        setGameTitle("Brandweerman Mark");
+        setSize(new Size(800, 600));
+    }
+
+    @Override
+    protected void setupScenes() {
+        startScreen = new Startscherm(this);
+        endScreen = new Eindscherm(this);
+
+        addScene(0, startScreen);
+        addScene(1, new Normal_Room(this));
+        addScene(2, endScreen);
+    }
+
+    public Player getPlayer() {
+        return _player;
+    }
+
+    public int getWinstLevel() {
+        return winst_level;
+    }
+
+    public int getRonde() {
+        return ronde;
+    }
+
+
+    public List<Rooms> getRooms() {
+        return rooms;
+    }
 }
--- src/main/java/com/github/hanyaeger/Brandweerman_Mark/entities/player/modifiers/cursed/Dubbel_Schade_Half_Hp.java
+++ src/main/java/com/github/hanyaeger/Brandweerman_Mark/entities/player/modifiers/cursed/Dubbel_Schade_Half_Hp.java
@@ -1,4 +1,5 @@
 package com.github.hanyaeger.Brandweerman_Mark.entities.player.modifiers.cursed;
+import com.github.hanyaeger.Brandweerman_Mark.entities.player.modifiers.Cursed;
 
 public class Dubbel_Schade_Half_Hp extends Cursed {
 
