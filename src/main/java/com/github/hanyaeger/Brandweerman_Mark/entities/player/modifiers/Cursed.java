04eb6c2cc071ed2705c022e3c851e65c9e86c6b6
DownsizedO <galaxytijn@hotmail.com>
2025-04-02 14:35:02 +0200

    shit

--- src/main/java/com/github/hanyaeger/Brandweerman_Mark/entities/player/modifiers/Cursed.java
+++ src/main/java/com/github/hanyaeger/Brandweerman_Mark/entities/player/modifiers/Cursed.java
@@ -2,5 +2,9 @@ package com.github.hanyaeger.Brandweerman_Mark.entities.player.modifiers;
 
 public abstract class Cursed extends Modifiers {
 
+    public Cursed(String naam, String type, String effect, String nadeel) {
+        super(naam, type, effect, nadeel);
+    }
+
     public abstract void Geef_Bonus();
 }
--- src/main/java/com/github/hanyaeger/Brandweerman_Mark/entities/player/modifiers/Explosie_enemy.java
+++ src/main/java/com/github/hanyaeger/Brandweerman_Mark/entities/player/modifiers/Explosie_enemy.java
@@ -2,6 +2,10 @@ package com.github.hanyaeger.Brandweerman_Mark.entities.player.modifiers;
 
 public class Explosie_enemy extends Cursed {
 
+    public Explosie_enemy(String naam, String type, String effect, String nadeel) {
+        super(naam, type, effect, nadeel);
+    }
+
     @Override
     public void Geef_Bonus() {}
 }
